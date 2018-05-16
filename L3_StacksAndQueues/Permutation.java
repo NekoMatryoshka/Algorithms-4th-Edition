c9e2b56277112d13be778793bb47e26d045118e6
NekoMatryoshka <30490996+NekoMatryoshka@users.noreply.github.com>
2018-05-16 13:31:04 +0100

    Add files via upload

--- L3_StacksAndQueues/Permutation.java
+++ L3_StacksAndQueues/Permutation.java
@@ -5,9 +5,8 @@ public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> queue = new RandomizedQueue<>();
-        String[] strs = StdIn.readAllStrings();
-        for (String str: strs)
-            queue.enqueue(str);
+        while (!StdIn.isEmpty())
+            queue.enqueue(StdIn.readString());
         for (int i = 0; i < k; i++)
             StdOut.println(queue.dequeue());
     }
